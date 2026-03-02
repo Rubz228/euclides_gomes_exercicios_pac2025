0d07945d8ad9b6501aa539cd8c981fec4da534f1
Euclides <rubengomes0228@gmail.com>
2026-03-02 23:31:01 +0000

    Exercicio8

--- Exercicios If.py
+++ Exercicios If.py
@@ -48,9 +48,9 @@ else:
 
 #---------Exercicio5----------------
 
-num1 = int(input("\nDigite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-num3 = int(input("Digite o terceiro número: "))
+num1 = int(input("\nEscreve o primeiro número: "))
+num2 = int(input("Escreve o segundo número: "))
+num3 = int(input("Escreve o terceiro número: "))
 
 
 if num1 >= num2 and num1 >= num3:
@@ -82,9 +82,9 @@ print(f"Decrescente: {maior}, {meio}, {menor}")
 
 #---------Exercicio7----------------
 
-nota1 = float(input("\nDigite a nota da primeira prova: "))
-nota2 = float(input("Digite a nota da segunda prova: "))
-nota3 = float(input("Digite a nota da terceira prova: "))
+nota1 = float(input("\nEscreve a nota do 1º Teste: "))
+nota2 = float(input("Escreve a nota do 2º Teste: "))
+nota3 = float(input("Escreve a nota do 3ª Teste: "))
 
 media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10
 
@@ -94,3 +94,45 @@ if media >= 6:
     print("Aprovado")
 else:
     print("Reprovado")
+
+#---------Exercicio8----------------
+
+n1 = float(input("Digite a nota do aluno 1: "))
+n2 = float(input("Digite a nota do aluno 2: "))
+n3 = float(input("Digite a nota do aluno 3: "))
+n4 = float(input("Digite a nota do aluno 4: "))
+n5 = float(input("Digite a nota do aluno 5: "))
+n6 = float(input("Digite a nota do aluno 6: "))
+n7 = float(input("Digite a nota do aluno 7: "))
+n8 = float(input("Digite a nota do aluno 8: "))
+n9 = float(input("Digite a nota do aluno 9: "))
+n10 = float(input("Digite a nota do aluno 10: "))
+
+soma = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
+media = soma / 10
+
+acima_media = 0
+
+if n1 >= media:
+    acima = acima_media + 1
+if n2 >= media:
+    acima = acima_media + 1
+if n3 >= media:
+    acima = acima_media + 1
+if n4 >= media:
+    acima = acima_media + 1
+if n5 >= media:
+    acima = acima_media + 1
+if n6 >= media:
+    acima = acima_media + 1
+if n7 >= media:
+    acima = acima_media + 1
+if n8 >= media:
+    acima = acima_media + 1
+if n9 >= media:
+    acima = acima_media + 1
+if n10 >= media:
+    acima = acima_media + 1
+
+print(f"\nMédia: {media:.1f}")
+print(f"Alunos com nota acima/igual da média: {acima}")
