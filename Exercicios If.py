b9714a37fac95cb5776322afefa0c6850fd345c7
Euclides <rubengomes0228@gmail.com>
2026-03-02 23:18:48 +0000

    Exercicos5_7

--- Exercicios If.py
+++ Exercicios If.py
@@ -48,3 +48,49 @@ else:
 
 #---------Exercicio5----------------
 
+num1 = int(input("\nDigite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+
+if num1 >= num2 and num1 >= num3:
+    maior = num1
+elif num2 >= num1 and num2 >= num3:
+    maior = num2
+else:
+    maior = num3
+
+
+if num1 <= num2 and num1 <= num3:
+    menor = num1
+elif num2 <= num1 and num2 <= num3:
+    menor = num2
+else:
+    menor = num3
+
+
+if (num1 != maior and num1 != menor):
+    meio = num1
+elif (num2 != maior and num2 != menor):
+    meio = num2
+else:
+    meio = num3
+
+print(f"\nCrescente: {menor}, {meio}, {maior}")
+print(f"Decrescente: {maior}, {meio}, {menor}")
+
+
+#---------Exercicio7----------------
+
+nota1 = float(input("\nDigite a nota da primeira prova: "))
+nota2 = float(input("Digite a nota da segunda prova: "))
+nota3 = float(input("Digite a nota da terceira prova: "))
+
+media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10
+
+print(f"\nMédia: {media:.1f}")
+
+if media >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
