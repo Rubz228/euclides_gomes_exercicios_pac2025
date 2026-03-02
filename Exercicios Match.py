f7f2aa94c8c793ff3e4de7936b565937021f9b8e
Euclides <rubengomes0228@gmail.com>
2026-03-02 21:28:04 +0000

    exercicio5

--- Exercicios Match.py
+++ Exercicios Match.py
@@ -1,27 +1,39 @@
-opc=input("Segunda- dia util , Terça- dia util , Quarta- dia util , Quinta- dia util , Sexta- dia util , Sabado- Fim de semana , Domingo- Fim de semana")
-
+#-----------------------------Exercicio1----------------------------    
+opc = input("Escolha um dia da semana:\n"
+            "Segunda - dia útil\n"
+            "Terça - dia útil\n"
+            "Quarta - dia útil\n"
+            "Quinta - dia útil\n"
+            "Sexta - dia útil\n"
+            "Sábado - Fim de semana\n"
+            "Domingo - Fim de semana\n"
+            "Diz o dia da semana: ")
 match opc:
 
     case "Segunda":
-        print("dia util")
+        print("dia util\n")
     case "Terça":
-        print("dia util")
+        print("dia util\n")
     case "Quarta":
-        print("dia util")
+        print("dia util\n")
     case "Quinta":
-        print("dia util")
+        print("dia util\n")
     case "Sexta":
-        print("dia util")
+        print("dia util\n")
     case "Sábado":
-        print("Fim de semana")
+        print("Fim de semana\n")
     case "Domingo":
-        print("Fim de semana")
+        print("Fim de semana\n")
     case _:
-        print("Opção não existe")
+        print("Opção não existe\n")
 
-#-------------------------------------------------------------------------------------
+#-----------------------------Exercicio2----------------------------   
 
-Nota=input("Excelente- 90 ou > , Bom- 70-89 , Sufciente- 50-69 , Insufciente- <50")
+Nota=input("Excelente- 90 ou >\n"
+           "Bom- 70-89\n"
+           "Sufciente- 50-69\n"
+           "Insufciente- <50\n"
+           "Escolha a nota:")
 
 match Nota:
 
@@ -36,24 +48,44 @@ match Nota:
     case _:
         print("Opção não existe")
 
-#--------------------------------------
+#-----------------------------Exercicio3----------------------------   
 
-tipo = input("Escolhe a transação: ")
-valor = int(input("Escreve o valor: "))
+transaçao = input("\nEscolhe a transação: ")
+valor = int(input("\nEscreve o valor: \n"))
 
-pedido = {"tipo": tipo, "valor": valor}
+pedido = {"tipo": transaçao, "valor": valor}
 
 match pedido["tipo"]:
     case "compra":
-        print("Compra de", pedido["valor"],)
+        print("Compra de\n", pedido["valor"],)
     case "venda":
-        print("Venda de", pedido["valor"],)
+        print("Venda de\n", pedido["valor"],)
     case _:
-        print("Pedido desconhecido")
+        print("Pedido desconhecido\n")
 
-#--------------------------------------
+#-----------------------------Exercicio5----------------------------   
 
+msg = input("Digite uma mensagem:\n"
+            "olá - Saudação\n"
+            "bom dia - Saudação\n"
+            "tchau - Despedida\n"
+            "adeus - Despedida\n"
+            "És quem? - Pergunta\n"
+            "Diz a mensagem: ")
 
+match msg:
+    case "ola":
+        print("Saudação")
+    case "bom dia":
+        print("Saudação")
+    case "tchau":
+        print("Despedida")
+    case "adeus":
+        print("Despedida")
+    case _ if msg.endswith("?"):
+        print("Pergunta")
+    case _:
+        print("Mensagem genérica")
     
 
     
