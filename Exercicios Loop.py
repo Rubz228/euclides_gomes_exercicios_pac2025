8221e39576ec2f8d6178bdf8544f58c7c56103cb
Euclides <rubengomes0228@gmail.com>
2026-03-04 16:43:22 +0000

    Exercicio1

---
+++ Exercicios Loop.py
@@ -0,0 +1,11 @@
+#--------------Exercicio 1-----------------
+def par_impar():
+
+    for i in range(31):
+
+        if i % 2 == 0:
+            print(i,"par")
+        else:
+            print( i,"Impar")
+
+par_impar()
