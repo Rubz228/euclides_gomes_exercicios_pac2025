de5696dd9c3a45b79267ced42dab27502be248b5
Euclides <rubengomes0228@gmail.com>
2026-03-09 15:02:16 +0000

    Exercicio_4_5_6

--- Exercicios Loop.py
+++ Exercicios Loop.py
@@ -1,4 +1,5 @@
 #--------------Exercicio 1-----------------
+
 def par_impar():
     for i in range(31):
         if i % 2 == 0:
@@ -37,3 +38,43 @@ def media():
     print("A média da turma é:", media)
 
 media()
+
+#--------------Exercicio 4-----------------
+
+def primo():
+    num = int(input("Digite um número inteiro: "))
+    
+    if num <= 1:
+        print(num, "não é primo")
+    else:
+        e_primo = True  
+        
+        for i in range(2, num):
+            if num % i == 0:
+                e_primo = False 
+                break 
+        
+        if e_primo:
+            print(num, "é um número primo")
+        else:
+            print(num, "não é um número primo")
+
+primo() 
+
+#--------------Exercicio 5-----------------
+
+def Imprimir():
+    for i in range(1, 10001):
+        print(i, end=" ")
+
+Imprimir()
+
+#--------------Exercicio 6-----------------
+
+def mostrar_primos():
+    lista_primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    
+    for p in lista_primos:
+        print(p, end=" ")
+
+mostrar_primos()
