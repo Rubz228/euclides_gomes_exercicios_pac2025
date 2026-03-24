71da774674935a6e71e8c919feccd30817009cb6
Euclides <rubengomes0228@gmail.com>
2026-03-24 21:44:12 +0000

    registro.txt exercicios

--- exercicio_regex_dados_txt.py
+++ exercicio_regex_dados_txt.py
@@ -26,3 +26,24 @@ with open('extraidos.txt', 'w', encoding='utf-8') as f:
 
 emails_pt = [e for e in emails if e.endswith('.pt')]
 print(emails_pt)
+
+#---------------------------------------------------------------
+
+import re
+from datetime import datetime
+
+with open('registos.txt', 'r', encoding='utf-8') as f:
+    conteudo = f.read()
+
+
+nifs = re.findall(r'NIF:\s*(\d{9})', conteudo)
+
+datas = re.findall(r'\d{2}/\d{2}/\d{4}', conteudo)
+
+codigos_postais = re.findall(r'\d{4}-\d{3}', conteudo)
+
+
+dominios = re.findall(r'https?://(?:www\.)?([^/\s|]+)', conteudo)
+
+
+nifs_validos = [n for n in nifs if n[0] in '123568']
